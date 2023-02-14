e80ac24a039e140aac0d1b68a906e7e80a34f3c4
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-14 16:32:41 -0500

    Create spinner using setTimeout

---
+++ spinner1.js
@@ -0,0 +1,41 @@
+// we can use \r to return cursor back to the beginning of the line that we are on - make animation stay in same spot
+
+//print this out to see what happens and getter better sence of what \r does 
+// process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1700);
+
